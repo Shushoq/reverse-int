4516e69a21315a7a886726b1f5afb8a74365e565
shushoq <wywkoff@gmail.com>
2021-02-12 18:27:02 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n < 0 ? n = n * (-1) : n
+    // n = n.toString()
+    // return +(n.split('').reverse().join(''))
+    let result = ''
+    n < 0 ? n = n * (-1) : n
+    while (n) {
+        result = result + n % 10
+        n = Math.floor(n / 10)
+    }
+    return +result
 }
